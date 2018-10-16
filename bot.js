18bf95cb5008ce4cd357e0d9ff0f4cdc2c0f4f2f
Noob828 <44176466+Noob828@users.noreply.github.com>
2018-10-16 21:17:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-بظconst Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
   client.user.setGame('f!help |Over Power Shop','https://www.twitch.tv/peery13');
